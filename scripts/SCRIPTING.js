418baad9a9e1de5ab31a5d690bab64fff7dc6c6e
Raphael <rkhan461@mtroyal.ca>
2023-04-02 21:23:29 -0600

    Finalizing

--- scripts/SCRIPTING.js
+++ scripts/SCRIPTING.js
@@ -13,9 +13,9 @@ function myFunction() {
 
 
   if(!darkToggle){
-  document.getElementById('intro').style.backgroundImage = 'url("images/dark_mode.jpg")';
+  document.getElementById('intro').style.backgroundImage = 'var(--dark_mode_main)';
   document.body.style.color = 'white';
-  document.getElementById('dark_mode').style.color="white"
+  document.getElementById('dark_mode').style.color="var(--night_text)";
   
   document.body.style.backgroundColor = "var(--main_night)";
   
@@ -38,8 +38,8 @@ function myFunction() {
   else
   
   {
-    document.getElementById('intro').style.backgroundImage = 'linear-gradient(rgba(18, 121, 70, 0.1), rgba(40, 92, 102, 0.11)), url("images/img2.avif")';
-    document.body.style.color = 'black';
+    document.getElementById('intro').style.backgroundImage = 'var(--day_image)';
+    document.body.style.color = "var(--day_text)";
    
     elements.forEach(element => {
       element.style.backgroundColor= "var(--header_day)";
